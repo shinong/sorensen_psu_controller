36ea25b1f8460ef3734b101d2edb795e93a9b631
Shinong <shinongmao@gmail.com>
2020-11-03 15:06:19 -0500

    add contorl to left side (unstable, need test on rpi)

--- code/main.py
+++ code/main.py
@@ -15,12 +15,12 @@ import json
 
 
 class Psu():
-    def __init__(self, port= None):
+    def __init__(self, port= None, left= False):
         if port:
             print("the port is" + port)
             self.port = port
             self.status_log_check()
-            self.load_config()
+            self.load_config(left)
             self.serial_connection_start()
             self.psu_init()
             self.mqtt_config()
@@ -44,10 +44,14 @@ class Psu():
             print("Serial connection failed")
         
 
-    def load_config(self):
+    def load_config(self, left):
         config = Config_handler()
-        self.Amp_set = config.Amp_set
-        self.Q_set = config.Q_set
+        if left:
+            self.Amp_set = config.Amp_set
+            self.Q_set = config.Q_set
+        else:
+            self.Amp_set = config.Amp_set_left
+            self.Q_set = config.Q_set_left
         self.broker_address = config.broker_address
         self.mqtt_username = config.mqtt_username
         self.mqtt_password = config.mqtt_password
@@ -137,18 +141,19 @@ class App(tk.Frame):
         self.search_serial_ports()
         self.config = Config_handler()
         self.create_widgets()
-        self.started = False
+        self.started_right = False
+        self.started_left = False
     
     def create_widgets(self):
         #right buttons
-        self.start_button_right = tk.Button(self, text= "Start", bg= "green", command= lambda:threading.Thread(target=self.start_command).start())
+        self.start_button_right = tk.Button(self, text= "Start", bg= "green", command= lambda:threading.Thread(target=self.start_command_right).start())
         self.start_button_right.grid(row = 5, column = 2)
-        self.stop_button_right = tk.Button(self, text= "Force Stop", bg= "red", command= self.stop_command)
+        self.stop_button_right = tk.Button(self, text= "Force Stop", bg= "red", command= self.stop_command_right)
         self.stop_button_right.grid(row = 6, column = 2)
         #left buttons
-        self.start_button_left = tk.Button(self, text= "Start", bg= "grey")
+        self.start_button_left = tk.Button(self, text= "Start", bg= "green", command= lambda:threading.Thread(target=self.start_command_left).start())
         self.start_button_left.grid(row=5, column= 0)
-        self.stop_button_left = tk.Button(self, text= "Force Stop", bg= "grey")
+        self.stop_button_left = tk.Button(self, text= "Force Stop", bg= "red", command= self.stop_command_left)
         self.stop_button_left.grid(row= 6, column= 0)
         #shared bottoms
         self.port_refresh_button = tk.Button(self, text= "Refresh", bg = "yellow", command= self.update_serial_ports)
@@ -170,9 +175,9 @@ class App(tk.Frame):
         self.est_label_left.grid(row=7, column= 0)
         self.est_time_left = ttk.Label(self, text= "N/A")
         self.est_time_left.grid(row=8, column= 0)
-        self.set_current_label_left = tk.Label(self, text = "Set current left {}".format(self.config.Amp_set))
+        self.set_current_label_left = tk.Label(self, text = "Set current left {}".format(self.config.Amp_set_left))
         self.set_current_label_left.grid(row = 0, column = 0)
-        self.set_Q_label_left = tk.Label(self, text = "Q left is set to {}".format(self.config.Q_set))
+        self.set_Q_label_left = tk.Label(self, text = "Q left is set to {}".format(self.config.Q_set_left))
         self.set_Q_label_left.grid(row = 1, column = 0)
         #right labels
         self.voltage_label_right = tk.Label(self,text = "0.00")
@@ -196,59 +201,95 @@ class App(tk.Frame):
         self.port_select_left["values"] = available_serial_ports
         self.port_select_right["values"] = available_serial_ports
 
-    def start_command(self):
-        if not self.started:
-            self.power_control = Psu(self.port_select_right.get())
-            self.power_control.running_status = True
-            self.started = True
-            self.current_label_right.after(1000, self.update_label)
-            self.update_est_time_label()
+    def start_command_right(self):
+        if not self.started_right:
+            self.power_control_right = Psu(self.port_select_right.get(),left= False)
+            self.power_control_right.running_status = True
+            self.started_right = True
+            self.current_label_right.after(1000, self.update_label_right)
+            self.update_est_time_label(left=False, amp_hour= self.config.Q_set,amp= self.config.Amp_set)
             #   power_control.running() func contains a while loop inside, any other starter code should be 
             #   placed above this
-            self.power_control.running()
+            self.power_control_right.running()
         else:
             print("already running")
 
-    def stop_command(self):
-        self.power_control.running_status = False
-        self.started = False
+    def stop_command_right(self):
+        self.power_control_right.running_status = False
+        self.started_right = False
+
+    def start_command_left(self):
+        if not self.started_left:
+            self.power_control_left = Psu(self.port_select_right.get(), left= True)
+            self.power_control_left.running_status = True
+            self.started_left = True
+            self.current_label_left.after(1000, self.update_label_left)
+            self.update_est_time_label(left= True, amp_hour= self.config.Q_set_left, amp= self.config.Amp_set_left)
+            #   power_control.running() func contains a while loop inside, any other starter code should be 
+            #   placed above this
+            self.power_control_left.running()
+        else:
+            print("already running")
+
+    def stop_command_left(self):
+        self.power_control_left.running_status = False
+        self.started_left = False
         
-    def update_label(self):
-        if self.started:
-            self.voltage_label_right["text"] = self.power_control.volt
-            self.current_label_right["text"] = self.power_control.curr
-            self.current_label_right.after(10000,self.update_label)
+    def update_label_right(self):
+        if self.started_right:
+            self.voltage_label_right["text"] = self.power_control_right.volt
+            self.current_label_right["text"] = self.power_control_right.curr
+            self.current_label_right.after(10000,self.update_label_right)
             try:
-                if not self.power_control.running_status:
-                    self.started = False
+                if not self.power_control_right.running_status:
+                    self.started_right = False
             except NameError:
                 pass
         else:
             self.voltage_label_right["text"] = "OFF"
             self.current_label_right["text"] = "OFF"
+
+    def update_label_left(self):
+        if self.started_left:
+            self.voltage_label_left["text"] = self.power_control_left.volt
+            self.current_label_left["text"] = self.power_control_left.curr
+            self.current_label_left.after(10000,self.update_label_left)
+            try:
+                if not self.power_control_left.running_status:
+                    self.started_left = False
+            except NameError:
+                pass
+        else:
+            self.voltage_label_left["text"] = "OFF"
+            self.current_label_left["text"] = "OFF"
     
-    def est_time_cal(self):
-        amp_hour = self.config.Q_set
-        amp = self.config.Amp_set
+    def est_time_cal(self, amp_hour, amp):
         hours = int(amp_hour/amp)
         minutes = (amp_hour%amp)/4.8*60
         start_time = datetime.datetime.now()
         return str(start_time + datetime.timedelta(hours = hours, minutes= minutes))
     
-    def update_est_time_label(self):
-        #self.est_time_left["text"] = self.est_time_cal()
-        self.est_time_right["text"] = self.est_time_cal()
+    def update_est_time_label(self,left,amp_hour,amp):
+        if left:
+            self.est_time_left["text"] = self.est_time_cal(amp_hour,amp)
+        else:
+            self.est_time_right["text"] = self.est_time_cal(amp_hour,amp)
         
 
 class Config_handler():
     def __init__(self):
         f = open("config.txt","r")
         config = f.read().splitlines()
-        self.Amp_set = float(config[0].split(":")[1])
-        self.Q_set = float(config[1].split(":")[1])
-        self.broker_address = config[2].split(":")[1]
-        self.mqtt_username = config[3].split(":")[1]
-        self.mqtt_password = config[4].split(":")[1]
+        # load the config parameters for the right side
+        self.Amp_set = float(config[1].split(":")[1])
+        self.Q_set = float(config[2].split(":")[1])
+        # load the config parameters for the left side
+        self.Amp_set_left = float(config[4].split(":")[1])
+        self.Q_set_left = float(config[5].split(":")[1])
+        # load the config parameters for mqtt client
+        self.broker_address = config[7].split(":")[1]
+        self.mqtt_username = config[8].split(":")[1]
+        self.mqtt_password = config[9].split(":")[1]
         f.close()
 
 
